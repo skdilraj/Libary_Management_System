6384e0e79e2cc6ab2212566278c1770720d6a020
skdilraj <anud7067@gmail.com>
2025-07-16 15:54:31 +0530

    folder structure add

--- src/app/components/admins/page.js
+++ src/app/components/admins/page.js
@@ -1,12 +1,19 @@
+<<<<<<< HEAD
 import Dashboard from "../layout/admins/dashBoard";
 import Sidebar from "../layout/admins/sideBar";
+=======
+>>>>>>> 591d2f7 (folder structure add)
 
 
 export default function Home() {
   return (
+<<<<<<< HEAD
     <main className="flex">
       <Sidebar/>
       <Dashboard/>
     </main>
+=======
+    <main></main>
+>>>>>>> 591d2f7 (folder structure add)
   );
 }
