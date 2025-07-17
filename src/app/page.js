57797420f8f46d2c04c147acb77c20435e03d98b
skdilraj <anud7067@gmail.com>
2025-07-17 12:56:25 +0530

    page.js change

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 
 export default function Home() {
   return (
     <main></main>
   );
-}
-=======
-import Image from "next/image";
-=======
 
->>>>>>> 591d2f7 (folder structure add)
+}
 
-export default function Home() {
-  return (
-    <main></main>
-  );
-<<<<<<< HEAD
-}
->>>>>>> b40fd92 (Initial commit from Create Next App)
-=======
-}
->>>>>>> 591d2f7 (folder structure add)
