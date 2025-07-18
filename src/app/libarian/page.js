9598a3a15a6b52bab7828a38cdc38dafcfba3bd0
skdilraj <anud7067@gmail.com>
2025-07-18 20:17:45 +0530

    Libarian folder structure

---
+++ src/app/libarian/page.js
@@ -0,0 +1,8 @@
+import Sidebar from "../components/libarian/sidebar"
+export default function Home() {
+    return (
+        <main className="flex flex-col md:flex-row">
+           <Sidebar/> 
+        </main>
+    )
+}
