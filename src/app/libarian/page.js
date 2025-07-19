216cf5d5f9f33edbbd8c78e468d5f24643cf0afb
skdilraj <anud7067@gmail.com>
2025-07-20 00:33:51 +0530

    make responsive

--- src/app/libarian/page.js
+++ src/app/libarian/page.js
@@ -2,7 +2,7 @@ import Sidebar from "../components/libarian/sidebar";
 import Home from "../components/libarian/home";
 export default function Librarian(){
     return(
-        <main className="flex">
+        <main className="flex flex-col md:flex-row">
             <Sidebar/>
             <Home/>
         </main>
